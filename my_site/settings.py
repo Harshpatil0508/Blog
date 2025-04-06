80b6fd14780ad2d3206c05dac65badbc50ba1202
Harshpatil0508 <patilharshvardhan9090@gmail.com>
2025-04-06 12:56:19 +0530

    updated settings.py

--- my_site/settings.py
+++ my_site/settings.py
@@ -29,7 +29,7 @@ DEBUG = False
 # DEBUG = getenv("IS_DEVELOPMENT", True)
 
 # ALLOWED_HOSTS = [*]
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
